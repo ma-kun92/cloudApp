a8ffb9125548a0084654b8bcda1e8a9629d38625
ma-kun92 <c1nakata@yahoo.co.jp>
2019-04-14 19:39:57 +0900

    edit image_uploader

--- app/uploaders/video_uploader.rb
+++ app/uploaders/video_uploader.rb
@@ -32,24 +32,24 @@ class VideoUploader < CarrierWave::Uploader::Base
   version :thumb do
     process resize_to_fit: [50, 50]
   end
-  version :screenshot do
-    process :screenshot
-    def full_filename (for_file = model.logo.file)
-      "screenshot.jpg"
-    end
-  end
+  # version :screenshot do
+  #   process :screenshot
+  #   def full_filename (for_file = model.logo.file)
+  #     "screenshot.jpg"
+  #   end
+  # end
 
-  def screenshot
-    tmpfile = File.join(File.dirname(current_path), "tmpfile")
+  # def screenshot
+  #   tmpfile = File.join(File.dirname(current_path), "tmpfile")
 
-    File.rename(current_path, tmpfile)
+  #   File.rename(current_path, tmpfile)
 
-    movie = FFMPEG::Movie.new(tmpfile)
-    movie.screenshot(current_path + ".jpg", {resolution: '512x312' }, preserve_aspect_ratio: :width)
-    File.rename(current_path + ".jpg", current_path)
+  #   movie = FFMPEG::Movie.new(tmpfile)
+  #   movie.screenshot(current_path + ".jpg", {resolution: '512x312' }, preserve_aspect_ratio: :width)
+  #   File.rename(current_path + ".jpg", current_path)
 
-    File.delete(tmpfile)
-  end
+  #   File.delete(tmpfile)
+  # end
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
   # def extension_whitelist
