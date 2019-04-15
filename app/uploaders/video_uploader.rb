4d7241c047844b71f0ae7db164d1cb68adaff9bb
ma-kun92 <c1nakata@yahoo.co.jp>
2019-04-16 01:08:09 +0900

    a

--- app/uploaders/video_uploader.rb
+++ app/uploaders/video_uploader.rb
@@ -13,14 +13,6 @@ class VideoUploader < CarrierWave::Uploader::Base
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
-def move_to_cache
-    true
-  end
-
-  def move_to_store
-    true
-  end
-end
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url(*args)
   #   # For Rails 3.1+ asset pipeline compatibility:
