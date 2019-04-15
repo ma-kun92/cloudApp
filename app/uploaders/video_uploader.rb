032077817040a9adc35c37b2f53df1fc6a5b5791
ma-kun92 <c1nakata@yahoo.co.jp>
2019-04-16 00:31:12 +0900

    edit uploader

--- app/uploaders/video_uploader.rb
+++ app/uploaders/video_uploader.rb
@@ -6,7 +6,7 @@ class VideoUploader < CarrierWave::Uploader::Base
   # Choose what kind of storage to use for this uploader:
   storage :file
   # storage :fog
-  require 'streamio-ffmpeg'
+  # require 'streamio-ffmpeg'
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
